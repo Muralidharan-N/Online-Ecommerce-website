947fb9d9acf0ff1c8c0a76bb1308539a9a4723d0
Muralidharan N <117349353+muralinithiya@users.noreply.github.com>
2024-07-18 12:34:16 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <title>JD Fermetures</title>
+    <link rel="stylesheet" href="CSS/design.css">
+
+</head>
+<body>
+    <table width="100%" >
+        <tr>
+            <td><img src="IMG/Image2.png"> </td>
+            <td>
+                <table>
+                    <tr>
+                        <td><img src="IMG/icone1.png"></td>
+                        <td><p>4A rue de l'Eau qui Court</p>
+                            <p>68850 Staffelfelden</p></td>
+                    </tr>
+     </table>
+            </td>
+            <td>
+                <table>
+                    <tr>
+                        <td><img src="IMG/icone2.png"></td>
+                        <td>06 82 07 58 12</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+    <nav>
+        <ul>
+            <li class="active1"><a href="index.php">ACCUEIL</a></li>
+            <li><a href="nosServices.php">NOS SERVICES</a></li>
+            <li><a href="fenetre.html">FENETRE ET VOLETS</a></li>
+            <li><a href="portes.php">PORTES</a></li>
+            
+            <li><a href="pros-com.html">PROS - COMMERCE</a></li>
+            <li><a href="contact.php">CONTACT</a></li>
+            <li><a href="login.php">LOGIN</a></li>
+        </ul>
+    </nav>
