63c8051de373490b2befdf2d002c09561f69a796
Muralidharan N <117349353+muralinithiya@users.noreply.github.com>
2024-07-18 12:51:42 +0530

    Create portes.php

---
+++ portes.php
@@ -0,0 +1,96 @@
+<?php include 'header.php'; ?>
+
+<link rel="stylesheet" href="CSS/design-nosServices.css">
+
+ <!-- Font Awesome CDN-->
+ <link
+ rel="stylesheet"
+ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+/>
+
+
+<section>
+    <div class="row">
+        <h2 class="section-heading">Nos Porduits Portes</h2>
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                    <img class="img-card" src="IMG/Image3.png">
+                    </div>
+                    <h3>FENÊTRES</h3>
+                    <h4 style="color: red;">300 dt</h4>
+                
+        </div>
+        
+        </div>
+
+    
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                <img class="img-card" src="IMG/Image3.png">
+                    </div>
+                    <h3>VOLETS</h3>
+                    <h4 style="color: red;">500 dt</h4>
+        </div>
+    </div>
+
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                <img class="img-card" src="IMG/Image3.png">
+                    </div>
+                    <h3>PORTES</h3>
+                    <h4 style="color: red;">700 dt</h4>
+        </div>
+
+    </div>
+
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                <img class="img-card" src="IMG/Image3.png">
+                    </div>
+                    <h3>PORTAILS</h3>
+                    <h4 style="color: red;">900 dt</h4>
+                
+        </div>
+    </div>
+
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                <img class="img-card" src="IMG/Image3.png">
+                    </div>
+                    <h3>CLÔTURES</h3>
+                    <h4 style="color: red;">800 dt</h4>
+                
+        </div>
+
+    </div>
+    
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                <img class="img-card" src="IMG/Image3.png">
+                    </div>
+                    <h3>STORES - PERGOLAS</h3>
+                    <h4 style="color: red;">900 dt</h4>
+                
+        </div>
+
+        </div>
+
+    
+
+    </div>
+
+</section>
+
+
+
+
+<?php include 'footer.php'; ?>
