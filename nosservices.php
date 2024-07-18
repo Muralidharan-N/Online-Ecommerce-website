4789e96c2600a75fbfc48fb072bdb19ebf0b0ec9
Muralidharan N <117349353+muralinithiya@users.noreply.github.com>
2024-07-18 12:37:51 +0530

    Create nosservices.php

---
+++ nosservices.php
@@ -0,0 +1,100 @@
+<?php include 'header.php'; ?>
+
+<link rel="stylesheet" href="CSS/design-nosServices.css">
+
+ <!-- Font Awesome CDN-->
+ <link
+ rel="stylesheet"
+ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+/>
+
+
+<section>
+    <div class="row">
+        <h2 class="section-heading">Nos Services</h2>
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                    <i class="fas fa-cart-shopping"></i>
+                    </div>
+                    <h3>Achat</h3>
+                    <p>
+                      Achat de votre occasions produits .
+                    </p>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                    <i class="fas fa-cart-shopping"></i>
+                    </div>
+                    <h3>Vente</h3>
+                    <p>
+                      Vente de plusieurs gamme de produits.
+                    </p>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                    <i class="fas fa-wrench"></i>
+                    </div>
+                    <h3>Reparation</h3>
+                    <p>
+                      Reparation des tous les modeles.
+                    </p>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                    <i class="fas fa-check"></i>
+                    </div>
+                    <h3>Granatie</h3>
+                    <p>
+                      Grantie plus de 3 ans.
+                    </p>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                    <i class="fas fa-truck"></i>
+                    </div>
+                    <h3>Livraison</h3>
+                    <p>
+                      Livraison sur tous la Tunisie.
+                    </p>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <div class="icon-wrapper">
+                    <i class="fas fa-money-bill-1"></i>
+                    </div>
+                    <h3>Facilite de Payement</h3>
+                    <p>
+                      Payment sur plusieurs tranches.
+                    </p>
+            </div>
+        </div>
+
+
+      
+
+    </div>
+
+</section>
+
+
+
+
+<?php include 'footer.php'; ?>
